5eddde8293306ce09411b45656e52a0d348d1b46
Sean Ruffolo <SeanRuffolo@terminal-de-shifkey.local>
2019-02-27 14:42:50 -0800

    initial setup of components/reducer

---
+++ src/Actions/index.js
@@ -0,0 +1,4 @@
+export const changeSong = (song) => ({
+  type: 'CHANGE_SONG',
+  song
+})
---
+++ src/Components/SongList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import 
+
+class SongList extends React.Component {
+
+  render () {
+    return <div>songlist</div>
+  }
+}
+
+export default
